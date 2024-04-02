816f988d1c4930dba84bccd0154a71febda56a14
wraymond2017 <wraymond2017@outlook.com>
2024-04-01 22:00:51 -0400

    final

--- src/js/views/addcontact.js
+++ src/js/views/addcontact.js
@@ -28,30 +28,29 @@ export const AddContact = () => {
     };
     return (
         <div style={{ width: '50rem', marginTop: '2rem' }}>
-            <h2 className="d-flex justify-content-center">Add New Contact</h2>
+            <h2 className="d-flex justify-content-center">Add your new contact here</h2>
             <form onSubmit={handleSubmit}>
             <div className="mb-3 d-flex flex-wrap">
             <label for="name" className="form-label">Full Name</label>
-                <input className="form-control" type="text" value={name} onChange={(e) => setName(e.target.value)} required placeholder="Enter Full Name" />
+                <input className="form-control" type="text" value={name} onChange={(e) => setName(e.target.value)} required placeholder="Enter Your Full Name" />
             </div>
             <div className="mb-3">
             <label for="email" className="form-label">Email</label>
-                <input className="form-control" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required placeholder="Enter Email" />
+                <input className="form-control" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required placeholder="Enter Your Email" />
             </div>
             <div className="mb-3">
             <label for="phone" className="form-label">Phone</label>
-                <input className="form-control" type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} required placeholder="Enter Phone" />
+                <input className="form-control" type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} required placeholder="Enter Your Phone Number" />
             </div>
             <div className="mb-3">
             <label for="address" className="form-label">Address</label>
-                <input className="form-control" type="text" value={address} onChange={(e) => setAddress(e.target.value)} required placeholder="Enter Address" />
+                <input className="form-control" type="text" value={address} onChange={(e) => setAddress(e.target.value)} required placeholder="Enter your Mailing Address" />
             </div>
                 <button type="submit" className="btn btn-primary" style={{ width: '50rem' }}>Save</button>
                 {/* (?)link above do not fetch */}
             </form>
             <Link to="/">
- 				or get back to contacts
-			</Link>
+Go back to the homepage			</Link>
         </div>
     );
 };
